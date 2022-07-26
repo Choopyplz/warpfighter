e0e8dafa1d79c380fa95a14421e1f5d2c17bf710
Chad Cooper <chadcooper102@gmail.com>
2022-07-26 14:21:30 -0400

    finalizing gallery and general look of site

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// --------------Hamburger Menu-------------
+
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
@@ -11,20 +13,12 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
     navMenu.classList.remove("active");
 }));
 
-// function validation(){
-//     var name = document.getElementById("name").value;
-//     var comment = document.getElementById("comment").value;
-
-//     if (document.myform.name.value == "") {
-//         document.getElementById('errors').innerHTML="*Please enter a username*";
-//         return false;
-//    }
-// }
+// -----------------Gallery-----------------
 
 const gallery = document.getElementById('gallery');
 const popup = document.getElementById('popup');
 const selectedImage = document.getElementById('selectedImage');
-const imageIndexes = [1,2,3,4];
+const imageIndexes = [1,2,3,5];
 const selectedIndex = null;
 
 imageIndexes.forEach(i => {
@@ -33,13 +27,19 @@ imageIndexes.forEach(i => {
     image.alt = `Space Battle Panel ${i}`;
     image.classList.add('galleryImg');
 
-    iamge.addEventListener('click', () => {
-        //popup
+    image.addEventListener('click', () => {
+        popup.style.transform = `translateY(0)`;
+        selectedImage.src = `/images/panel-${i}.png`;
     })
 
     gallery.appendChild(image);
 })
 
-// image.addEventListener('click', () => {
-//     selectedImage.src = "galleryImg';
-// });
+popup.addEventListener('click', () => {
+    popup.style.transform = `translateY(-100%)`;
+    popup.src = '';
+    popup.alt = '';
+})
+
+
+
