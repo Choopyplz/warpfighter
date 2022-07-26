72d6c01e5c6ebb38035fc0ca64298ff63483ea02
Chad Cooper <chadcooper102@gmail.com>
2022-07-26 10:44:55 -0400

    progress on image gallery

--- script.js
+++ script.js
@@ -10,3 +10,36 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }));
+
+// function validation(){
+//     var name = document.getElementById("name").value;
+//     var comment = document.getElementById("comment").value;
+
+//     if (document.myform.name.value == "") {
+//         document.getElementById('errors').innerHTML="*Please enter a username*";
+//         return false;
+//    }
+// }
+
+const gallery = document.getElementById('gallery');
+const popup = document.getElementById('popup');
+const selectedImage = document.getElementById('selectedImage');
+const imageIndexes = [1,2,3,4];
+const selectedIndex = null;
+
+imageIndexes.forEach(i => {
+    const image = document.createElement('img');
+    image.src = `/images/panel-${i}.png`;
+    image.alt = `Space Battle Panel ${i}`;
+    image.classList.add('galleryImg');
+
+    iamge.addEventListener('click', () => {
+        //popup
+    })
+
+    gallery.appendChild(image);
+})
+
+// image.addEventListener('click', () => {
+//     selectedImage.src = "galleryImg';
+// });
